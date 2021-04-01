38b5add24786dd3980efd4387941fa85eea35184
yohan fairfort <yohan.fairfort@gmail.com>
2021-04-01 16:17:08 +0200

    Refacto

--- BNPKata/IZones.cs
+++ BNPKata/IZones.cs
@@ -1,57 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace BNPKata
+﻿namespace BNPKata
 {
     public interface IZones
     {
         (Zone, Zone, int pricing) CheapestTripFrom(string startStation, string endStation);
-        int To(string endStation);
-        int Cost(Zone startStation, Zone endStation);
-    }
-
-    public class Zones : IZones
-    {
-        private readonly IEnumerable<Zone> _zones;
-
-        public Zones(IEnumerable<Zone> zones)
-        {
-            _zones = zones;
-        }
-
-        public (Zone, Zone, int pricing) CheapestTripFrom(string startStation, string endStation)
-        {
-            Zone inside = _zones
-                .Where(z => z.ContainStation(startStation) && z.ContainStation(endStation))
-                .OrderBy(x=> x.ArgPriceOfInsideTrip)
-                .FirstOrDefault();
-            
-            (Zone inside, Zone outside, int pricing) insideToOutside = _zones
-                .Where(z => z.ContainStation(startStation))
-                .OrderBy(z=> z.CheapestZoneToTravelAndPrice(EndZones(endStation)).pricing)
-                .Select(z => (z, z.CheapestZoneToTravelAndPrice(EndZones(endStation)).zone, z.CheapestZoneToTravelAndPrice(EndZones(endStation)).pricing))
-                .FirstOrDefault();
-
-            if (inside == null)
-                return insideToOutside;
-            if (inside.ArgPriceOfInsideTrip > insideToOutside.pricing)
-                return insideToOutside;
-            return (inside, inside, inside.ArgPriceOfInsideTrip);
-        }
-
-        private IEnumerable<Zone> EndZones(string endStation)
-        {
-            return _zones.Where(x => x.ContainStation(endStation));
-        }
-
-        public int To(string endStation)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int Cost(Zone startStation, Zone endStation)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
